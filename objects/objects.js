ce5b7529d8e462176a83f23f24f8e981f454ec28
ainaladevi <ainaladevi@gmail.com>
2026-05-29 11:26:50 +0530

    js tasks updated

--- objects/objects.js
+++ objects/objects.js
@@ -23,3 +23,20 @@ let userPerson={
  arr2.push(10);
  console.log(arr1);
  console.log(arr2);
+
+ //4.
+
+ let obj = {
+    name : "devi",
+    age : 22,
+    Address : {
+         city : "Guntur",
+         State : "AP"
+    }
+ }
+  let obj1 = {
+    ...obj
+  }
+  obj1.Address.city = "vjd"
+  console.log(obj);
+  console.log("copied", obj1);
